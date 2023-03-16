20f6518069f52c2d532ecf5a70324a54213a3ffc
FREDVUNI <fredvuni809@gmail.com>
2023-03-16 13:06:53 +0300

    Add table to contacts

--- client/src/components/Contact.tsx
+++ client/src/components/Contact.tsx
@@ -76,7 +76,7 @@ const Contact = (props: Props) => {
         }
     }
   return (
-    <div className='grid grid-cols-2 min-h-full bg-white px-6 sm:py-30 lg:px-8'>
+    <div className='grid lg:grid-cols-2 min-h-full bg-white px-6 sm:py-30 lg:px-8'>
         <div className="px-12 py-12 md:px-12 text-gray-800 lg:text-left grid lg:grid-cols-1 gap-12 items-center">
             <div className="container justify-center">
                 <p className='text-xl text-black'>{category.category}</p>
@@ -99,6 +99,44 @@ const Contact = (props: Props) => {
                </div>
             </div>
             <AddContact category={category.category}/>
+        </div>
+        <div className="flex lg:flex-col px-12 py-12 md:px-12">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+                <table className="min-w-full text-left text-sm font-light">
+                <thead className="border-b font-medium dark:border-neutral-500">
+                    <tr>
+                    <th scope="col" className="px-6 py-4">#</th>
+                    <th scope="col" className="px-6 py-4">First</th>
+                    <th scope="col" className="px-6 py-4">Last</th>
+                    <th scope="col" className="px-6 py-4">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                    <td className="whitespace-nowrap px-6 py-4">Mark</td>
+                    <td className="whitespace-nowrap px-6 py-4">Otto</td>
+                    <td className="whitespace-nowrap px-6 py-4">@mdo</td>
+                    </tr>
+                    <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                    <td className="whitespace-nowrap px-6 py-4">Jacob</td>
+                    <td className="whitespace-nowrap px-6 py-4">Thornton</td>
+                    <td className="whitespace-nowrap px-6 py-4">@fat</td>
+                    </tr>
+                    <tr className="border-b dark:border-neutral-500">
+                    <td className="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                    <td className="whitespace-nowrap px-6 py-4">Larry</td>
+                    <td className="whitespace-nowrap px-6 py-4">Wild</td>
+                    <td className="whitespace-nowrap px-6 py-4">@twitter</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
   )
